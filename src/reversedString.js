024aef90340e71a1f1e19b44e5fd7f3fd6fd549c
al.n.pavlov <al.n.pavlov@tinkoff.ru>
2024-01-30 18:44:40 +0500

    reversedString

---
+++ src/reversedString.js
@@ -0,0 +1,25 @@
+function reverseString(str) {
+    let reversedString = '';
+    for (let i = str.length; i > 0; i--) {
+        reversedString += str.substring(i, i-1);
+    }
+    return reversedString;
+}
+
+function reverseString2(str) {
+    let reversedString = '';
+    for (let i = str.length-1; i >= 0; i--) {
+        reversedString += str.charAt(i);
+    }
+    return reversedString;
+}
+
+function reverseString3(str) {
+    return str.split('').reverse().join('');
+}
+
+
+
+console.log(reverseString('Abrau'));
+console.log(reverseString2('Abrau'));
+console.log(reverseString3('Abrau'));
